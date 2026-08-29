f8253de15bbad25ff3e972b6858abc910dd29606
harsh13exe <harshdhiman74949@gmail.com>
2026-08-29 23:45:03 +0530

    finalize requirements with conditional audioop-lts

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -5,28 +5,29 @@ import os
 DOWNLOAD_DIR = 'downloades'
 os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
-def download_youtube_audio(url:str)->str:
-    output_path = os.path.join(DOWNLOAD_DIR,"%(title)s.%(ext)s")
+def download_youtube_audio(url: str) -> str:
+    output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
     ydl_opts = {
         "format": "bestaudio/best",
         "outtmpl": output_path,
         "postprocessors": [
             {
-                "key": "FFmpegExtractAudio", 
-                "preferredcodec": "wav", 
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
                 "preferredquality": "192"
             }
         ],
-        "quiet":True,
-        "js_runtimes": {
-            "deno": {}
+        "quiet": True,
+        "extractor_args": {
+            "youtube": {
+                "player_client": ["android", "web"]
+            }
         },
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
         filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
     return filename
-
 
 def convert_to_wav(input_path:str)->str:
     """Convert any audio/video file to WAV format using pydub."""
